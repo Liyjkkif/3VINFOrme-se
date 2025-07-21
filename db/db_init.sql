f083075784bebb4f4a3a7535d3e314848a97efed
Adriely Daiany <162337039+Liyjkkif@users.noreply.github.com>
2025-07-21 14:09:57 +0000

    sbr

--- db/db_init.sql
+++ db/db_init.sql
@@ -1,8 +1,6 @@
--- Cria o banco
 CREATE DATABASE IF NOT EXISTS turma_db;
 USE turma_db;
 
--- Cria a tabela de usuários
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL UNIQUE,
